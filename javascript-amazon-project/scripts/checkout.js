58df4f2a26e5174ef544bbe5ce3c2c30b58c13e2
keiske0408 <kieske0408@gmail.com>
2025-07-26 15:54:15 +0800

    delete product HTML when clicking delete

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
   });
   cartSummaryHTML +=   
   `
-  <div class="cart-item-container">
+  <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
       Delivery date: Tuesday, June 21
     </div>
@@ -42,7 +42,7 @@ cart.forEach((cartItem) => {
             Update
           </span>
           <span class="delete-quantity-link link-primary js-delete-link
-          " data-product-id = "${matchingProduct.id}">
+          "data-product-id = "${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -106,5 +106,11 @@ document.querySelectorAll('.js-delete-link')
   link.addEventListener('click', ()=>{
     const { productId } = link.dataset
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.remove();
   })
 })
