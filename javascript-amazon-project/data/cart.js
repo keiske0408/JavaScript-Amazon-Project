039274c74d022afdcb1c473cd38b9a322cdf274e
keiske0408 <kieske0408@gmail.com>
2025-07-26 16:14:58 +0800

    save cart in local storage

--- javascript-amazon-project/data/cart.js
+++ javascript-amazon-project/data/cart.js
@@ -1,4 +1,5 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) ||
+[
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
@@ -8,6 +9,10 @@ export let cart = [
     quantity: 1
   }
 ];
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -26,6 +31,8 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -38,4 +45,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart
+
+  saveToStorage();
 }
