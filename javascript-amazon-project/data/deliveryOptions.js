2dc44c0185b4425f0eca1596288677c03e61176e
keiske0408 <kieske0408@gmail.com>
2025-09-03 11:40:12 +0800

    create function delivery calculation that skips weekends

--- javascript-amazon-project/data/deliveryOptions.js
+++ javascript-amazon-project/data/deliveryOptions.js
@@ -1,5 +1,5 @@
-
-
+import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
+ const today = dayjs();
 export const deliveryOptions = [
   {
     id: "1",
@@ -21,11 +21,32 @@ export const deliveryOptions = [
 export function getDeliveryOption(deliveryOptionId) {
   let deliveryOption;
 
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        deliveryOption = option;
-      }
-    });
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      deliveryOption = option;
+    }
+  });
 
-  return deliveryOption || deliveryOptions[0]
+  return deliveryOption || deliveryOptions[0];
 }
+
+function isWeekend(date){
+  const dayOfWeek = date.format('dddd');
+  return dayOfWeek === "Saturday" || dayOfWeek === "Sunday";
+}
+
+  export function calculateDeliveryDate(deliveryOption) {
+   let remainingDays = deliveryOption.deliveryDays;
+   let deliveryDate = today;
+
+   while (remainingDays > 0){
+    deliveryDate = deliveryDate.add(1,'day')
+
+    const weekDay = deliveryDate.format('dddd')
+    if (weekDay !== 'Saturday' && weekDay !== 'Sunday'){
+      remainingDays--
+    }
+   }
+
+   return deliveryDate.format('dddd,MMMM D')
+} 
--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -7,10 +7,9 @@ import {
 } from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import { hello } from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js";
-import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
-import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
+import { deliveryOptions, getDeliveryOption,calculateDeliveryDate } from "../../data/deliveryOptions.js";
 import { renderPaymentSummary } from "./paymentSummary.js";
+import { renderCheckoutHeader } from "./checkoutHeader.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -24,9 +23,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId)
 
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-    const dateString = deliveryDate.format("dddd, MMMM D");
+    const dateString = calculateDeliveryDate(deliveryOption);
 
     cartSummaryHTML += `
   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
@@ -82,11 +79,9 @@ export function renderOrderSummary() {
     let html = "";
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
+      
+      const dateString = calculateDeliveryDate(deliveryOption);
 
-      const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-
-      const dateString = deliveryDate.format("dddd, MMMM D");
       const priceString =
         deliveryOption.priceCents === 0
           ? "Free"
@@ -133,6 +128,7 @@ export function renderOrderSummary() {
       cartItemContainer.classList.add("is-editing-quantity");
 
       console.log(cartItemContainer);
+      
     });
   });
 
@@ -144,13 +140,19 @@ export function renderOrderSummary() {
 
     link.addEventListener("click", () => {
       handleUpdateQuantity(productId, quantityInput);
+      renderCheckoutHeader();
+      renderOrderSummary();
       renderPaymentSummary();
+      
     });
 
     quantityInput.addEventListener("keydown", (event) => {
       if (event.key === "Enter") {
         handleUpdateQuantity(productId, quantityInput);
+        renderCheckoutHeader();
+        renderOrderSummary();
         renderPaymentSummary();
+       
       }
     });
   });
@@ -184,13 +186,10 @@ export function renderOrderSummary() {
         const { productId } = link.dataset;
         removeFromCart(productId);
 
-        const container = document.querySelector(
-          `.js-cart-item-container-${productId}`
-        );
-
-        container.remove();
-
+        renderCheckoutHeader();
+        renderOrderSummary();
         renderPaymentSummary();
+        
       });
       calculateCartQuantity();
     });
