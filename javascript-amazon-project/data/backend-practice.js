b4a8c1673cdfd4973222727a5d0a480356bfa9f1
keiske0408 <kieske0408@gmail.com>
2025-09-05 18:09:20 +0800

    learn promises and convert code to promises

--- javascript-amazon-project/data/backend-practice.js
+++ javascript-amazon-project/data/backend-practice.js
@@ -2,7 +2,7 @@ const xhr = new XMLHttpRequest();
 
 xhr.addEventListener('load',()=>
   {
-    console.log(xhr.response)
+    //console.log(xhr.response)
   });
 
 xhr.open('GET', 'https://supersimplebackend.dev');
--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,13 +1,57 @@
-import {renderOrderSummary} from "./checkout/orderSummary.js";
+import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 import { loadProducts } from "../data/products.js";
-import '../data/car.js';
-import  '../data/backend-practice.js';
+import { loadCart } from "../data/cart.js";
+import "../data/car.js";
+import "../data/backend-practice.js";
 //import '../data/cart-class.js';
 
-loadProducts(() => {
-  renderCheckoutHeader();
-  renderOrderSummary();
-  renderPaymentSummary();
+Promise.all([
+  new Promise((resolve) => {
+    loadProducts(() => {
+      resolve("value1");
+    });
+  }),
+  new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
+    });
+  })
+
+]).then(()=>{
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
 });
+
+/*
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve("value1");
+  });
+})
+  .then((value) => {
+    return new Promise((resolve) => {
+      loadCart(() => {
+        resolve();
+      });
+    });
+  })
+  .then(() => {
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+*/
+
+
+/*
+loadProducts(() => {
+  loadCart(()=>{
+    renderCheckoutHeader();
+    renderOrderSummary();
+    renderPaymentSummary();
+  })
+});
+*/
